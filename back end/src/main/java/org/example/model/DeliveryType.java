6ca75e253600496e189e87b2eebb9e3a46f0ce5a
i24s0235 <daniilkazimirov991@gmail.com>
2026-04-17 18:11:06 +0500

    fix: delivery

--- back end/src/main/java/org/example/model/DeliveryType.java
+++ back end/src/main/java/org/example/model/DeliveryType.java
@@ -1,6 +1,10 @@
 package org.example.model;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
 public enum DeliveryType {
-    CDEK,
-    DELOVYE_LINII
+    DELOVYE_LINII, CDEK;
+
+    @JsonCreator
+    public static DeliveryType fromString(String value) {
+        return valueOf(value.toUpperCase());
+    }
 }
